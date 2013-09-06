9559bc751d9573a9006fba9c0416088a3e5f864b
longbill.cn@gmail.com <liuchun643210>
2013-09-07 01:14:37 +0800

    init import

---
+++ app.js
@@ -0,0 +1,252 @@
+#! /usr/bin/env node
+
+var proxy = require('./proxy');
+var fs = require('fs');
+
+
+
+
+//copy example rule file
+if (process.argv[2] == '-init')
+{
+	var ex = fs.readFileSync(__dirname+'/rule-example.ini');
+	fs.writeFileSync("devproxy.ini",ex);
+	console.log("example rule file generated as ./devproxy.ini");
+	process.exit();
+}
+
+
+
+var defaultRuleFile = '';
+if (process.argv[2]) defaultRuleFile = process.argv[2];
+
+if (defaultRuleFile == '')
+{
+	console.log("Welcome to devproxy!\n\
+Usage:\n\
+	to generate example rule file at current working directory,\n\
+	you can use the following command:\n\
+		\n\
+		$ devproxy -init\n\
+		\n\
+	to start devproxy with rule file, use the following:\n\
+	\n\
+		$ devproxy path/to/devproxy.ini\n\
+		\n\
+Have fun!\n\
+");
+	process.exit();
+}
+
+
+
+
+
+
+var RULES = [];
+var SETTINGS = {};
+
+
+proxy.filter = function(req,res,next)
+{
+	if (req.method != 'GET' && req.method != 'POST')
+	{
+		next();
+		return;
+	}
+
+	var filePath = '';
+	for(var i=0,rule; rule=RULES[i]; i++)
+	{
+		if (req.method == rule.method && req.url && rule.reg && req.url.match(rule.reg))
+		{
+			if (rule.match.match(/\/\*\*\*$/) && rule.file.match(/\/\*\*\*$/))
+			{
+				var basePath = rule.match.replace(/\*\*\*$/,'');
+				var rPath = req.url.replace(basePath,'');
+				rPath = rPath.split('?').shift();
+				filePath = rule.file.replace(/\*\*\*$/,rPath);
+			}
+			else if (rule.match.match(/\/\*$/) && rule.file.match(/\/\*$/))
+			{
+				var baseName = basename(req.url);
+				filePath = rule.file.replace(/\*$/,baseName);
+			}
+			else
+			{
+				filePath = rule.file;
+			}
+		}
+	}
+
+
+	if (filePath)
+	{
+		fs.exists(filePath,function(exists)
+		{
+			if (exists)
+			{
+				fs.readFile(filePath,function(err,data)
+				{
+					if (err)
+					{
+						console.log('local file not found: '+filePath);
+						res.end("HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n");
+					}
+					else
+					{
+						console.log('local file: '+filePath);
+						res.write("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: "+data.length+"\r\n");
+						res.write("Content-Type: "+getContentType(filePath)+"\r\n\r\n");
+		                res.end(data);
+					}
+				});
+			}
+			else
+			{
+				next();
+			}
+		});
+	}
+	else
+	{
+		next();
+	}
+};
+
+
+
+
+fs.exists(defaultRuleFile,function(exists)
+{
+	if (exists)
+	{
+		syncRules(function()
+		{
+			fs.watchFile(defaultRuleFile,{persistent:true,interval:3000},syncRules);
+			var port = SETTINGS.LISTEN_PORT || 2000;
+			port = parseInt(port,10);
+			proxy.listen(port);
+		});
+	}
+	else
+	{
+		console.log('Error: can not find rule file '+defaultRuleFile);
+		process.exit(0);
+	}
+});
+
+
+function syncRules(cb)
+{
+	fs.readFile(defaultRuleFile, function (err, data)
+    {
+        if (!err)
+        {
+        	RULES = [];
+        	SETTINGS = {};
+			var s = data.toString();
+			var lines = s.split("\n");
+			for(var i=0;i<lines.length;i++)
+			{
+				var line = lines[i];
+				if (line.match(/^\s*\#/)) continue;
+				line = line.replace(/^\s+|\s+$/g,'');
+				if (!line) continue;
+				var p = line.split(/\s+/);
+
+
+				if (p[0] == 'SET')
+				{
+					SETTINGS[p[1]] = p[2];
+					continue;
+				}
+
+
+				if (p[0] && p[1])
+				{
+					var rule = {};
+					rule.reg = createURLReg(p[0]);
+					rule.match = p[0];
+					rule.file = p[1];
+					rule.method = p[2] || 'GET';
+					RULES.push(rule);
+				}
+			}
+			console.log('rule file '+defaultRuleFile+' has '+ RULES.length + ' rules');
+			syncSetting();
+			if (typeof cb == 'function') cb();
+		}
+		else
+		{
+			console.log('reading rules file error');
+		}
+    });
+}
+
+function syncSetting()
+{
+	if (SETTINGS['HTTPPROXY_HOST'] && SETTINGS['HTTPPROXY_PORT'])
+	{
+		proxy.HTTPProxy = { host: SETTINGS['HTTPPROXY_HOST'], port: parseInt(SETTINGS['HTTPPROXY_PORT'],10) };
+		console.log("SET HTTPPROXY to "+proxy.HTTPProxy.host+':'+proxy.HTTPProxy.port);
+	}
+	else
+	{
+		proxy.HTTPProxy = null;
+	}
+
+	if (SETTINGS['CACHE_FILE_TYPE'])
+	{
+		var arr = SETTINGS['CACHE_FILE_TYPE'].split(',');
+		var ftypes = {};
+		for(var i=0,ftype; ftype=arr[i]; i++)
+		{
+			ftype = ftype.toLowerCase().replace(/^[\s\t]+|[\s\t]+$/g,'');
+			if (ftype) ftypes[ftype] = true;
+		}
+		proxy.cacheFileTypes = ftypes;
+		console.log('SET CACHE FILE TYPES to '+SETTINGS['CACHE_FILE_TYPE'] );
+	}
+	else
+	{
+		proxy.cacheFileTypes = {};
+		proxy.clearCache();
+	}
+}
+
+function createURLReg(line)
+{
+	line = line.replace(/([\.\/\:\-\?\&\[\]\(\)\,\=\%])/g,"\\$1");
+	line = line.replace(/\*{3}/g,'.*');
+	line = line.replace(/\*/g,'[^\\/\\s\\r\\n]*');
+	return new RegExp('^'+line+'$');
+}
+
+function basename(url)
+{
+	if (!url) return '';
+	var l = url.split('/').pop();
+	return l.split('?').shift();
+}
+
+
+
+function getContentType(url)
+{
+    if (url.match(/\.css\b/i))
+    {
+        return 'text/css; charset=UTF-8';
+    }
+    else if (url.match(/\.js\b/i))
+    {
+        return 'text/javascript; charset=UTF-8';
+    }
+    else
+    {
+        return 'text/html; charset=utf-8';
+    }
+}
+
+
+
---
+++ proxy.js
@@ -0,0 +1,212 @@
+var net = require('net');
+var fs = require('fs');
+var local_port = 2000;
+
+
+exports.HTTPProxy = null;  // { host: "localhost", port:8000 }
+exports.cacheFileTypes = {};
+exports.clearCache = function() {};
+
+exports.filter = function(req,res,next)
+{
+    next();
+};
+
+exports.listen = function(port)
+{
+    if (port) local_port = port;
+
+    var CACHE = {};
+
+	exports.clearCache = function()
+	{
+		CACHE = {};
+	};
+	
+    net.createServer(function (client)
+    {
+        var buffer = new Buffer(0);
+        client.on('data',function(data)
+        {
+            buffer = buffer_add(buffer,data);
+            if (buffer_find_body(buffer) == -1) return;
+            var req = parse_request(buffer);
+            if (req === false) return;
+            client.removeAllListeners('data');
+
+
+            exports.filter(req, client, function()
+            {
+                if (req.method == 'GET')
+                {
+                    var ftype = req.url.split('?').shift().split('.').pop().toLowerCase();
+                    if ( exports.cacheFileTypes[ftype] && CACHE[req.path])
+                    {
+                        client.end(CACHE[req.path]);
+                    }
+                    else
+                    {
+                        relay_connection(req);
+                    }
+                }
+				else
+				{
+					relay_connection(req);
+				}
+            });
+
+        });
+
+        //从http请求头部取得请求信息后，继续监听浏览器发送数据，同时连接目标服务器，并把目标服务器的数据传给浏览器
+        function relay_connection(req)
+        {
+            //console.log(req.method+' '+req.host+':'+req.port);
+
+            //如果请求不是CONNECT方法（GET, POST），那么替换掉头部的一些东西
+            if (req.method != 'CONNECT')
+            {
+                //先从buffer中取出头部
+                var _body_pos = buffer_find_body(buffer);
+                if (_body_pos < 0) _body_pos = buffer.length;
+                var header = buffer.slice(0,_body_pos).toString('utf8');
+                //替换connection头
+                header = header.replace(/(proxy\-)?connection\:.+\r\n/ig,'')
+                        .replace(/Keep\-Alive\:.+\r\n/i,'')
+                        .replace("\r\n",'\r\nConnection: close\r\n');
+
+                buffer = buffer_add(new Buffer(header,'utf8'),buffer.slice(_body_pos));
+            }
+            
+            try
+            {
+                if (exports.HTTPProxy)
+                {
+                    var server = net.createConnection(exports.HTTPProxy.port,exports.HTTPProxy.host);
+                }
+                else
+                {
+                    var server = net.createConnection(req.port,req.host);
+                }
+            }catch(e)
+            {
+                return;
+            }
+            //交换服务器与浏览器的数据
+            client.on("data", function(data){ server.write(data); });
+
+            var server_response_buffer = new Buffer(0);
+            server.on("data", function(data)
+            {
+                server_response_buffer = buffer_add(server_response_buffer,data);
+                if (client.writable) client.write(data);
+            });
+
+            server.on('end',function()
+            {
+                if (req.method == 'GET')
+                {
+                    var ftype = req.url.split('?').shift().split('.').pop().toLowerCase();
+                    if (exports.cacheFileTypes[ftype])
+                    {
+                        var _body_pos = buffer_find_body(server_response_buffer);
+                        if (_body_pos > 0)
+                        {
+                            var header = server_response_buffer.slice(0,_body_pos).toString('utf8');
+                            header = header.replace("\r\n","\r\nFromcache: yes\r\n");
+                            server_response_buffer = buffer_add(new Buffer(header,'utf8'),server_response_buffer.slice(_body_pos));
+                        }
+                        CACHE[req.path] = server_response_buffer;
+                        console.log('PROXY cached: '+req.path);
+                    }
+                }
+            });
+
+            server.on('error',function()
+            {
+                console.log('PROXY error '+req.method+':'+req.host);
+                client.end();
+            });
+
+            if (req.method == 'CONNECT')
+                client.write(new Buffer("HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n"));
+            else
+                server.write(buffer);
+        }
+    }).listen(local_port);
+
+    console.log('PROXY server running at localhost:'+local_port);
+
+};
+
+
+
+
+
+//处理各种错误
+// process.on('uncaughtException', function(err)
+// {
+//     console.log("\nError!!!!");
+//     console.log(err);
+// });
+
+
+
+/**
+* 从请求头部取得请求详细信息
+* 如果是 CONNECT 方法，那么会返回 { method,host,port,httpVersion}
+* 如果是 GET/POST 方法，那么返回 { metod,host,port,path,httpVersion}
+*/
+function parse_request(buffer)
+{
+    var s = buffer.toString('utf8');
+    var method = s.split('\n')[0].match(/^([A-Z]+)\s/)[1];
+    if (method == 'CONNECT')
+    {
+        var arr = s.match(/^([A-Z]+)\s([^\:\s]+)\:(\d+)\sHTTP\/(\d\.\d)/);
+        if (arr && arr[1] && arr[2] && arr[3] && arr[4])
+            return { method: arr[1], host:arr[2], port:arr[3],httpVersion:arr[4] };
+    }
+    else
+    {
+        var arr = s.match(/^([A-Z]+)\s([^\s]+)\sHTTP\/(\d\.\d)/);
+        if (arr && arr[1] && arr[2] && arr[3])
+        {
+            var host = s.match(/Host\:\s+([^\n\s\r]+)/)[1];
+            if (host)
+            {
+                var _p = host.split(':',2);
+                return { method: arr[1], host:_p[0], port:_p[1]?_p[1]:80, path: arr[2], url: arr[2],httpVersion:arr[3] };
+            }
+        }
+    }
+    return false;
+}
+
+
+
+
+/**
+* 两个buffer对象加起来
+*/
+function buffer_add(buf1,buf2)
+{
+    var re = new Buffer(buf1.length + buf2.length);
+    buf1.copy(re);
+    buf2.copy(re,buf1.length);
+    return re;
+}
+
+/**
+* 从缓存中找到头部结束标记("\r\n\r\n")的位置
+*/
+function buffer_find_body(b)
+{
+    for(var i=0,len=b.length-3;i<len;i++)
+    {
+        if (b[i] == 0x0d && b[i+1] == 0x0a && b[i+2] == 0x0d && b[i+3] == 0x0a)
+        {
+            return i+4;
+        }
+    }
+    return -1;
+}
